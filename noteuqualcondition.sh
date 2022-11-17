3848b981f67d1d2debf864ed4d231aab521e4134
muralionline <muralidevops@gmail.com>
2022-11-17 15:21:49 +0000

    Not Euqal Condition

---
+++ noteuqualcondition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+name=("venky" "venky1" "venky3" "venky4" )
+nameone=("venky6" "venky7" "venky8" "venky9" )
+nametwo=("venky5")
+if [ $name != $nameone ] || [ $name == $nametwo ]
+then
+echo ".................................."
+else
+        "--------------------------------"
+fi
