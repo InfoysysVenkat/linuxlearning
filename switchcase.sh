4c3ff99c7ce18d0235e048e7bd6a32bb01e0649b
muralionline <muralidevops@gmail.com>
2022-11-18 08:10:43 +0000

    Switch Example

---
+++ switchcase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="HowLinux"
+ 
+case $name in
+        "Linux")
+                echo "Not quite right"
+                ;;
+        "HowLinux")
+                echo "That's right!"
+                ;;
+esac
